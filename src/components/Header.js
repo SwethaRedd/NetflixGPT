6b4ca7a9b207e95788e1d8c3464ce8b20ba78534
Swetha <swethareddy.nadipi@gmail.com>
2024-06-20 11:29:10 -0400

    call singout apiin header comp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,22 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../utils/firebase";
+import { signOut } from "firebase/auth";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    // signOut api : https://firebase.google.com/docs/auth/web/password-auth
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        navigate("/error");
+      });
+  };
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
@@ -14,7 +30,9 @@ const Header = () => {
           src="https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e"
           alt="user-icon"
         />
-        <button className="font-bold text-white">(Sign Out)</button>
+        <button className="font-bold text-white" onClick={handleSignOut}>
+          (Sign Out)
+        </button>
       </div>
     </div>
   );
