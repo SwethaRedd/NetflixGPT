1fb9e0392c054eff57de53dc7cf70cb2b845107b
Swetha <swethareddy.nadipi@gmail.com>
2024-06-26 14:52:57 -0400

    dispatch the movies results from the api response to the addNowPlayingMovies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
       API_OPTIONS
     );
     const json = await data.json();
-    console.log("json", json);
+    dispatch(addNowPlayingMovies(json.results));
   };
 
   useEffect(() => {
