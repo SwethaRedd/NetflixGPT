afa69170bd94cfb38524f9012a90bffff732e2b1
Swetha <swethareddy.nadipi@gmail.com>
2024-07-08 15:23:10 -0400

    styles update on the background video trailer and the tile component

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -6,8 +6,9 @@ const VideoBackground = ({ movieId }) => {
   useMovieTrailer(movieId);
 
   return (
-    <div>
+    <div className="w-screen">
       <iframe
+        className="w-screen aspect-video"
         src={"https://www.youtube.com/embed/" + trailerVideo?.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const VideoTitle = ({ title, description }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 w-1/4 text-lg">{description} </p>
       <div className="">
-        <button className="bg-gray-500 bg-opacity-50 rounded-md text-white text-xl p-4 px-12">
+        <button className="bg-white rounded-md text-black text-xl p-4 px-12 hover:bg-opacity-80">
           ▶️ Play
         </button>
         <button className="mx-2 bg-gray-500 bg-opacity-50 rounded-md text-white text-xl p-4 px-12">
